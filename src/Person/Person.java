c12b59c07a1faa38a2c64287f90029ddbf0584f2
Jan Kazubski <122796088+VrickPL@users.noreply.github.com>
2023-05-23 14:54:12 +0200

    Update Person.java

--- src/Person/Person.java
+++ src/Person/Person.java
@@ -4,11 +4,15 @@ public class Person {
     private static int personNumber = 1;
     private final int personIndex;
     private int age;
+    private boolean alive = true;
+    private boolean infected = false;
+    private String country;
 
 
-    public Person(int age) {
+    public Person(int age, String country) {
         this.personIndex = Person.personNumber++;
         this.age = age;
+        this.country = country;
     }
 
     public void setAge(int age) {
@@ -23,6 +27,30 @@ public class Person {
         return age;
     }
 
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+
+    public void setInfected(boolean infected) {
+        this.infected = infected;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public boolean isInfected() {
+        return infected;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
     public double ageMultiplier(){
         if(age<18){
             return 1;
@@ -33,6 +61,3 @@ public class Person {
         }
     }
 }
-
-
-;
