5102e8f2e099ed431a6b0c3ab7e8495cdffc8c56
Jan Kazubski <122796088+VrickPL@users.noreply.github.com>
2023-05-28 18:21:34 +0200

    Update Country.java

--- src/World/Country.java
+++ src/World/Country.java
@@ -1,3 +1,5 @@
+package World;
+
 public class Country {
     private String countryName;
     private int alivePeopleAmount;
@@ -17,40 +19,32 @@ public class Country {
         return alivePeopleAmount;
     }
 
-    public int getDeadPeopleAmount() {
-        return deadPeopleAmount;
-    }
-
-    public int getSickPeopleAmount() {
-        return sickPeopleAmount;
-    }
-
     public void setCountryName(String countryName) {
         this.countryName = countryName;
     }
 
-    public void someoneDied(int alivePeopleAmount, int sickPeopleAmount, int deadPeopleAmount) {
-        this.alivePeopleAmount = alivePeopleAmount-1;
-        this.sickPeopleAmount = sickPeopleAmount-1;
-        this.deadPeopleAmount = deadPeopleAmount+1;
+    public void someoneDied() {
+        alivePeopleAmount--;
+        sickPeopleAmount--;
+        deadPeopleAmount++;
     }
 
-    public void someoneBorn(int alivePeopleAmount) {
-        this.alivePeopleAmount = alivePeopleAmount+1;
+    public void someoneBorn() { //nie wiem czy to chcemy
+        alivePeopleAmount++;
     }
 
-    public void someoneIsSick(int sickPeopleAmount) {
-        this.sickPeopleAmount = sickPeopleAmount+1;
+    public void someoneIsSick() {
+        sickPeopleAmount++;
     }
 
-    public void someoneRecover(int sickPeopleAmount) {
-        this.sickPeopleAmount = sickPeopleAmount-1;
+    public void someoneRecover() {
+        sickPeopleAmount--;
     }
 
-    public void printInfo(){
+    public void printDetailInfo(){
         System.out.printf("In %s:\n", countryName);
-        System.out.printf("Is alive %d people\n", alivePeopleAmount);
-        System.out.printf("Died %d people\n", deadPeopleAmount);
-        System.out.printf("Is sick %d people\n", sickPeopleAmount);
+        System.out.printf("Alive - %d people\n", alivePeopleAmount);
+        System.out.printf("Died - %d people\n", deadPeopleAmount);
+        System.out.printf("Infected - %d people\n", sickPeopleAmount);
     }
 }
