0b30ff9f6f111895aca67e5820aed5351bddb2be
Jan Kazubski <122796088+VrickPL@users.noreply.github.com>
2023-05-31 10:16:41 +0200

    Update Person.java

--- src/World/Person/Person.java
+++ src/World/Person/Person.java
@@ -1,164 +1,126 @@
-package World;
+package World.Person;
 
-import World.Person.Person;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Random;
-import java.util.Scanner;
 
-
-public class World {
-
-    ArrayList<Country> allCountries = new ArrayList<>();
-    ArrayList<Person> people = new ArrayList<>();
-    private int peopleAlive = 0;
-    private int deadPeople = 0;
-    private int deadPeopleDuringThreeMonths = 0;
-    private int infectedPeople = 0;
-    private int infectedPeopleDuringThreeMonths = 0;
-    private int peopleTransportedDuringThreeMonths = 0;
-    private int recoveredPeopleDuringThreeMonths = 0;
-    private boolean vaccineFound = false;
-    private int threeMonthsCounter = 0;
-    Scanner scanner = new Scanner(System.in);
+public class Person {
+    private static int personNumber = 1;
+    private final int personIndex;
+    private int age;
+    private boolean alive = true;
+    private boolean infected = false;
+    private String country;
     protected Random random = new Random();
 
 
-    public int getPeopleAlive() {
-        return peopleAlive;
+    public Person(int age, String country) {
+        this.personIndex = Person.personNumber++;
+        this.age = age;
+        this.country = country;
     }
 
-    public boolean isVaccineFound() {
-        return vaccineFound;
+    public void setAge(int age) {
+        if (isAlive()) this.age = age+1;
     }
 
-    public void countryImplementing() throws FileNotFoundException {
-        File file = new File("CountriesPO.txt");
-        Scanner fileScanner = new Scanner(file);
-        Country country;
+    public int getPersonIndex() {
+        return personIndex;
+    }
 
-        int i = 0;
-        while(fileScanner.hasNext()){
-            String fileLine = fileScanner.nextLine();
-            String[] splittedFileLine = fileLine.split(" ");
-            country = new Country(splittedFileLine[0], Integer.parseInt(splittedFileLine[1]));
-            allCountries.add(country);
-            //creating countries from "country.txt" file
-            while(allCountries.get(i).getCountryName().contains("_")){
-                String[] twoWordCountry = allCountries.get(i).getCountryName().split("_");
-                allCountries.get(i).setCountryName(String.join(" ", twoWordCountry[0], twoWordCountry[1]));
+    public int getAge() {
+        return age;
+    }
+
+    public void setAlive(boolean alive) {
+        this.alive = alive;
+    }
+
+    public void setInfected(boolean infected) {
+        this.infected = infected;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    public boolean isInfected() {
+        return infected;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public double dieAgeMultiplier(){
+        if(age<18){
+            return 1;
+        } else if(age<60){
+            return 2;
+        } else{
+            return 5;
+        }
+    }
+    public boolean randomDie(){
+        if(isAlive() && isInfected()){
+            if(random.nextInt(10) + 1 <= 1* dieAgeMultiplier()){
+                setAlive(false);
+                return true;
             }
-            System.out.println("World.Country: " + allCountries.get(i).getCountryName());
-            System.out.printf("Alive: %d people\n", allCountries.get(i).getAlivePeopleAmount());
-            System.out.println(" ");
-            peopleAlive += allCountries.get(i).getAlivePeopleAmount();
+        }
+        return false;
+    }
 
-            //creating people for all countries
-            for (int j = 0; j < allCountries.get(i).getAlivePeopleAmount(); j++) {
-                Person newHuman = new Person((random.nextInt(140)+1), allCountries.get(i).getCountryName());
-                people.add(newHuman);
+    public boolean randomGetInfected(){
+        if(isAlive() && !isInfected()){
+            if(random.nextInt(10) + 1 <= 1* dieAgeMultiplier()){
+                setAlive(false);
+                return true;
             }
+        }
+        return false;
+    }
 
-            i++;
+    public boolean recover(){
+        if(isAlive() && isInfected()){
+            if(random.nextInt(100) + 1 <= 20/ dieAgeMultiplier()){
+                setAlive(false);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public double transportAgeMultiplier(){
+        if(age<18){
+            return 5;
+        } else if(age<60){
+            return 1;
+        } else{
+            return 5;
         }
     }
 
-    public void setVaccineFound(boolean vaccineFound) {
-        this.vaccineFound = vaccineFound;
-    }
-
-    public void randomVaccineFound(){
-        int maxValue = 100;
-        //if(liczbaMutacjiWirusa>0) maxValue *= 1.3;
-        //jesli wirus mutuje to tez sie zmniejsza szansa na szczepionke
-        if(random.nextInt(maxValue) == 1) setVaccineFound(true);
-    }
-
-    public void duringThreeMonths(){
-        threeMonthsCounter++;
-        deadPeopleDuringThreeMonths = 0;
-        infectedPeopleDuringThreeMonths = 0;
-        peopleTransportedDuringThreeMonths = 0;
-        recoveredPeopleDuringThreeMonths = 0;
-
-        //jesli ktos umrze lub zachoruje to zmieniaja sie dane w kraju
-        for (int i = 0; i < people.size(); i++) {
-            if(people.get(i).randomDie()) {
-                deadPeopleDuringThreeMonths++;
-                boolean toContinue = true;
-                for (int j = 0; toContinue; j++) {
-                    if(allCountries.get(j).getCountryName().equals(people.get(i).getCountry())){
-                        allCountries.get(j).someoneDied();
-                        toContinue = false;
-                    }
+    public boolean randomTransport(){
+        //TRZEBA UZUPELNIC
+        //wazne jeszcze ze jak ktos sie transportuje to zmniejsza sie liczba osob w kraju danym i zwieksza w tym nowym (w zarazonych i zywych ogolnie)
+        if(isAlive() && (random.nextInt(100) + 1<= 10/transportAgeMultiplier())){
+            int whichTransport = 1 + random.nextInt(3);
+            switch(whichTransport){
+                case 1 -> {
+                    //car
+                }
+                case 2 -> {
+                    //ship
+                }
+                case 3 -> {
+                    //plane
                 }
             }
-
-            if(people.get(i).randomGetInfected()) {
-                infectedPeopleDuringThreeMonths++;
-                boolean toContinue = true;
-                for (int j = 0; toContinue; j++) {
-                    if(allCountries.get(j).getCountryName().equals(people.get(i).getCountry())){
-                        allCountries.get(j).someoneIsSick();
-                        toContinue = false;
-                    }
-                }
-            }
-
-            if(people.get(i).recover()){
-                recoveredPeopleDuringThreeMonths++;
-                boolean toContinue = true;
-                for (int j = 0; toContinue; j++) {
-                    if(allCountries.get(j).getCountryName().equals(people.get(i).getCountry())){
-                        allCountries.get(j).someoneRecover();
-                        toContinue = false;
-                    }
-                }
-            }
-
-            if(people.get(i).randomTransport()) peopleTransportedDuringThreeMonths++;
+            return true;
         }
-        peopleAlive -= deadPeopleDuringThreeMonths;
-        deadPeople += deadPeopleDuringThreeMonths;
-        infectedPeople += infectedPeopleDuringThreeMonths;
-
-        //MUTACJA WIRUSA
-
-        randomVaccineFound();
-
-        printInfo();
-
-        if(threeMonthsCounter % 4 == 0 && threeMonthsCounter != 0) newYear();
-    }
-
-    public void printInfo(){
-        System.out.println("-".repeat(30));
-        System.out.println("Date: "); //dodac date co sie zmienia co 3 miesiace 
-        System.out.println("ALIVE - " + peopleAlive + " PEOPLE");
-        System.out.println("INFECTED - " + infectedPeople + " PEOPLE");
-        System.out.println("DIED - " + deadPeople + " PEOPLE");
-        System.out.println(" ");
-        System.out.println("During 3 months: ");
-        System.out.println("Infected - " + infectedPeopleDuringThreeMonths + " people");
-        System.out.println("Died - " + deadPeopleDuringThreeMonths + " people");
-        System.out.println("Recovered - " + recoveredPeopleDuringThreeMonths + " people");
-        System.out.println("People transported - " + peopleTransportedDuringThreeMonths);
-        System.out.println("-".repeat(30));
-        System.out.println(" ");
-        System.out.println("Do you want to see the details of every country?");
-        System.out.println("Press 'y' (yes) or 'n' (no): ");
-        if (scanner.nextLine().toLowerCase().equals("y")){
-            for (int i = 0; i < allCountries.size(); i++) {
-                allCountries.get(i).printDetailInfo();
-            }
-        }
-    }
-
-    public void newYear(){
-        for (int i = 0; i < people.size(); i++) {
-            people.get(i).setAge(people.get(i).getAge());
-        }
+        return false;
     }
 }
