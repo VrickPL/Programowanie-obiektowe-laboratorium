c89cba53122d3dd9e88ed2060c64e48b58a6828b
VrickPL <jkazubski@icloud.com>
2023-06-04 19:54:49 +0200

    creating default settings

--- src/World/Virus/ImplementingVirus.java
+++ src/World/Virus/ImplementingVirus.java
@@ -9,15 +9,26 @@ public class ImplementingVirus {
 
         System.out.print("Enter his name: ");
         String name= dataInput();
-        System.out.print("Enter time of disease: ");
-        int timeOfDisease=Integer.parseInt(dataInput());
-        System.out.print("Enter factor of Contagious: ");
-        double factorOfContagious=Double.parseDouble(dataInput());
-        System.out.print("Enter time of Mutation: ");
-        double timeOfMutation=Double.parseDouble(dataInput());
-        System.out.print("Enter lethality: ");
-        double lethality=Double.parseDouble(dataInput());
-        return new Virus(timeOfMutation, timeOfDisease,factorOfContagious,lethality,name);
+
+        System.out.println("Do you want to use default settings? Enter \"yes\" or \"no\"");
+        System.out.print("Enter: ");
+        String whatToDo = scanner.nextLine().toLowerCase();
+        if(whatToDo.equals("yes")){
+            return new Virus(1, 1, 1, 1, name);
+        } else if (whatToDo.equals("no")) {
+            System.out.print("Enter time of disease: ");
+            int timeOfDisease=Integer.parseInt(dataInput());
+            System.out.print("Enter factor of Contagious: ");
+            double factorOfContagious=Double.parseDouble(dataInput());
+            System.out.print("Enter time of Mutation: ");
+            double timeOfMutation=Double.parseDouble(dataInput());
+            System.out.print("Enter lethality: ");
+            double lethality=Double.parseDouble(dataInput());
+            return new Virus(timeOfMutation, timeOfDisease,factorOfContagious,lethality,name);
+        } else {
+            System.out.println("Wrong string entered. Using default settings.");
+            return new Virus(1, 1, 1, 1, name);
+        }
     }
     private static String dataInput(){
         Scanner scanner = new Scanner(System.in);
