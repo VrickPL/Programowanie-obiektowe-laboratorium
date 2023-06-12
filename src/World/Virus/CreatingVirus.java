9e4f1ff0acdb4ec5968ca495579fe09d47f30a27
Jan Kazubski <122796088+VrickPL@users.noreply.github.com>
2023-06-12 20:37:55 +0200

    Update CreatingVirus.java

--- src/World/Virus/CreatingVirus.java
+++ src/World/Virus/CreatingVirus.java
@@ -4,33 +4,33 @@ import java.util.Scanner;
 
 public class CreatingVirus {
     public static Virus creatingVirus() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Make your own Virus!");
 
         System.out.print("Enter his name: ");
-        String name= dataInput();
+        String name= takeDataFromKeyboard();
 
         System.out.println("Do you want to use default settings? Enter \"yes\" or \"no\"");
         System.out.print("Enter: ");
-        String whatToDo = scanner.nextLine().toLowerCase();
+        String whatToDo = takeDataFromKeyboard();
+
         if(whatToDo.equals("yes")){
             return new Virus(100, 50, 5, 5, name);
         } else if (whatToDo.equals("no")) {
             System.out.print("Enter time of disease (days): ");
-            int timeOfDisease=Integer.parseInt(dataInput());
+            int timeOfDisease=Integer.parseInt(takeDataFromKeyboard());
             System.out.print("Enter factor of Contagious (1 - 10): ");
-            double factorOfContagious=Double.parseDouble(dataInput());
+            double factorOfContagious=Double.parseDouble(takeDataFromKeyboard());
             System.out.print("Enter time of Mutation (days): ");
-            double timeOfMutation=Double.parseDouble(dataInput());
+            double timeOfMutation=Double.parseDouble(takeDataFromKeyboard());
             System.out.print("Enter lethality (1 - 10): ");
-            double lethality=Double.parseDouble(dataInput());
+            double lethality=Double.parseDouble(takeDataFromKeyboard());
             return new Virus(timeOfMutation, timeOfDisease,factorOfContagious,lethality,name);
         } else {
             System.out.println("Wrong string entered. Using default settings.");
             return new Virus(100, 50, 5, 5, name);
         }
     }
-    private static String dataInput(){
+    private static String takeDataFromKeyboard(){
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
         while (name.isEmpty()){
